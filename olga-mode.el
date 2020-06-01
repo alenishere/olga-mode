859de9becf46dd5425ffa661794c2745d17e1a1e
Alen Alex Ninan <alenishere@gmail.com>
2020-06-01 09:20:39 +0530

    First commit

---
+++ olga-mode.el
@@ -0,0 +1,30 @@
+;;; ~/.doom.d/extra/olga/olga-mode.el -*- lexical-binding: t; -*-
+;; Copyright (C) 2011-2018  Free Software Foundation, Inc.
+
+;; Author: Alen Alex Ninan <alenishere@gmail.com>
+;; Version: 0.01
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This package provides the `adaptive-wrap-prefix-mode' minor mode which sets
+;; the wrap-prefix property on the fly so that single-long-line paragraphs get
+;; word-wrapped in a way similar to what you'd get with M-q using
+;; adaptive-fill-mode, but without actually changing the buffer's text.
+
+;;; Code:
+
+(provide 'olga-mode)
+;;; olga-mode.el ends here
